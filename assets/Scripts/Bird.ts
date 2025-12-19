19fcc2ad1fed8db84a026712fcccbf66a155d30f
sunlifang1990 <982290835@qq.com>
2025-12-19 16:12:58 +0800

    fix:小鸟朝向动态调整

--- assets/Scripts/Bird.ts
+++ assets/Scripts/Bird.ts
@@ -6,6 +6,9 @@ export class Bird extends Component {
     
     private rgdBody: RigidBody2D = null;
 
+    @property
+    private rotationSpeed: number = 100;
+
     onLoad () {
         input.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
 
@@ -20,11 +23,16 @@ export class Bird extends Component {
     }
 
     update(deltaTime: number) {
-        
+        if (this.node.angle > -50) {
+            this.node.angle -= this.rotationSpeed * deltaTime;
+        }
     }
 
     private onTouchStart () {
+        //
         this.rgdBody.linearVelocity = new Vec2(0, 10);   
+
+        this.node.setRotationFromEuler(new Vec3(0, 0, 30));
         //this.rgdBody.applyForceToCenter(new Vec2(0, 2), true);
         //this.rgdBody.applyLinearImpulseToCenter(new Vec2(0, 10), true);
     }
