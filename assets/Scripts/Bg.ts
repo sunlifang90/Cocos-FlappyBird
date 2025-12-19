20e9b2b1976359c010365661c90b27a429a46c81
sunlifang1990 <982290835@qq.com>
2025-12-19 13:55:43 +0800

    fix:背景移动

--- assets/Scripts/Bg.ts
+++ assets/Scripts/Bg.ts
@@ -16,7 +16,26 @@ export class Bg extends Component {
     }
 
     update(deltaTime: number) {
-        
+        let moveDistance = this.moveSpeed * deltaTime;
+
+        const bg1Pos = this.bg1.position;
+        const bg2Pos = this.bg2.position;
+
+        let bg1x = bg1Pos.x - moveDistance;
+        if (bg1x < -730){
+            // 移动到右边
+            this.bg1.setPosition(bg2Pos.x+725, bg1Pos.y, 0);
+        } else {
+            this.bg1.setPosition(bg1x, bg1Pos.y, 0);
+        }
+
+        let bg2x = bg2Pos.x - moveDistance;
+        if (bg2x < -730){
+            // 移动到右边
+            this.bg2.setPosition(bg1Pos.x+725, bg2Pos.y, 0);
+        } else {
+            this.bg2.setPosition(bg2x, bg2Pos.y, 0);
+        }
     }
 }
 
